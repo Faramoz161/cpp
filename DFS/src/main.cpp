7140af6d2a70de453220330a3878e1dc7e7c9eb3
Faramoz Aliev <faric.exe@gmail.com>
2018-10-03 20:26:18 +0300

    DFS is done

--- DFS/src/main.cpp
+++ DFS/src/main.cpp
@@ -1,16 +1,45 @@
 #include <iostream>
 #include <vector>
-#include <iterator>
 
 using namespace std;
 
-int main(int argc, char const *argv[]) {
-    
-    vector<vector<int>> adjacency_list;
+void DFS(vector<vector<int>> &adj_list, vector<bool> &is_visited, int vertex) {
+    is_visited[vertex] = true;
+    cout << "Visited vertex " << vertex << endl;
 
-    for (auto i : adjacency_list) {
-        for (auto j : i)
-            cout << j << " ";
-        cout << endl;       
-    }
+    for (size_t i {0}; i < adj_list[vertex].size(); i++)
+        if (!is_visited[adj_list[vertex][i]])
+            DFS(adj_list, is_visited, adj_list[vertex][i]);
+    
+    return;
+}
+
+int main(int argc, char const *argv[]) {
+    /*
+    Init adjacency list
+    */
+
+    // For example
+    vector<vector<int>> adj_list{
+        {1, 2, 3},
+        {0, 4},
+        {0, 3},
+        {0, 2},
+        {1}
+    };
+
+    // Vertex attendance array
+    vector<bool> is_visited(adj_list.size(), false);
+
+    // DFS from the first vertex
+    int init_vertex {0};
+    
+    is_visited[init_vertex] = true;
+    cout << "Initial vertex " << init_vertex << endl;
+
+    for (size_t i {0}; i < adj_list[init_vertex].size(); i++)
+        if (!is_visited[adj_list[init_vertex][i]])
+            DFS(adj_list, is_visited, adj_list[init_vertex][i]);
+    
+    return 1;
 }
